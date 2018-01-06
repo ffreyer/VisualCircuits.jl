4432629f14b278eeda9c06ae2f241e66e0119436
F Freyer <frederic481994@hotmail.de>
2018-01-06 10:28:42 +0100

    Revised shift register

--- example/shfit_register.jl
+++ example/shfit_register.jl
@@ -10,12 +10,12 @@ begin
         :CLK1   :PRE1   :CLR1   :J1     :VCC    :CLK2   :PRE2   :CLR2
     ])
 
-    width, height = 11, 5.8
+    width, height = 11, 6.5
     jk1 = IC7476(3.5, 3.3)
     jk2 = IC7476(8.5, 3.3)
 
     # edges
-    le = 1
+    le = 0
     re = width
 
     # guide (not displayed)
@@ -30,16 +30,31 @@ begin
     power = compose(
         context(),
         connect((le, y), (re, y)),
-        connect(jk1[:PRE1], y, :y),    connect(jk2[:PRE1], y, :y),
+        #connect(jk1[:PRE1], y, :y),    connect(jk2[:PRE1], y, :y),
         connect(jk1[:VCC],  y, :y),    connect(jk2[:VCC],  y, :y),
-        connect(jk1[:PRE2], y, :y),    connect(jk2[:PRE2], y, :y),
+        #connect(jk1[:PRE2], y, :y),    connect(jk2[:PRE2], y, :y),
         dots(
-            (jk1[:PRE1][1], y),        (jk2[:PRE1][1], y),
+            #(jk1[:PRE1][1], y),        (jk2[:PRE1][1], y),
             (jk1[:VCC][1],  y),        (jk2[:VCC][1],  y),
-            (jk1[:PRE2][1], y),        (jk2[:PRE2][1], y),
+            #(jk1[:PRE2][1], y),        (jk2[:PRE2][1], y),
             color = "darkred"
         ),
         stroke("darkred")
+    )
+
+    y = 6
+    preset = compose(
+        context(),
+        connect(jk2[:PRE1], y, :y),
+        connect(jk2[:PRE2], y, :y),
+        connect(jk1[:PRE1], y, :y),
+        connect(jk1[:PRE2], y, :y),
+        dots(
+            (jk1[:PRE1][1], y), (jk1[:PRE2][1], y),
+            (jk2[:PRE1][1], y), (jk2[:PRE2][1], y),
+            color = "darkgreen"
+        ),
+        stroke("darkgreen")
     )
 
     y = 1.0
@@ -65,9 +80,9 @@ begin
         connect(jk2[:CLR2], (jk2[:CLR2][1], y), (x, y)),
         dots(
             (jk1[:CLR1][1], y), (jk1[:CLR2][1], y),
-            (jk2[:CLR1][1], y), color = "darkgreen"
+            (jk2[:CLR1][1], y), color = "purple"
         ),
-        stroke("darkgreen")
+        stroke("purple")
     )
 
     # clock
@@ -89,7 +104,7 @@ begin
     x = 1
     y1 = 4.4
     y2 = 2.2
-    inputs = compose(
+    jk_in = compose(
         context(),
         connect(jk1[:J1], (jk1[:J1][1], y1), (x, y1)),
         connect(jk1[:K1], (jk1[:K1][1], y2), (x, y2)),
@@ -135,16 +150,20 @@ begin
     # labels
     labels = compose(
         context(),
-        text(0.9, 5.6, "VCC", hright, vcenter),
+        # text(0.9, 5.6, "VCC", hright, vcenter),
         text(0.9, 5.2, "CLR", hright, vcenter),
         text(0.9, 4.8, "CLK", hright, vcenter),
         text(0.9, 4.4, "J", hright, vcenter),
         text(0.9, 2.2, "K", hright, vcenter),
-        text(0.9, 1.0, "GND", hright, vcenter),
+        # text(0.9, 1.0, "GND", hright, vcenter),
         text(2.3, 0.4, "OUT1", hcenter, vbottom),
         text(4.3, 0.4, "OUT2", hcenter, vbottom),
         text(7.3, 0.4, "OUT3", hcenter, vbottom),
         text(9.3, 0.4, "OUT4", hcenter, vbottom),
+        text(2.3, 6.2, "IN1", hcenter, vtop),
+        text(4.8, 6.2, "IN2", hcenter, vtop),
+        text(7.3, 6.2, "IN3", hcenter, vtop),
+        text(9.8, 6.2, "IN4", hcenter, vtop),
         fontsize(20pt), font("Helvetica-Bold"), linewidth(0mm)
     )
 
@@ -152,10 +171,10 @@ begin
         context(units = UnitBox(0, 0, width, height)),
         render(jk1), render(jk2),
 
-        #box,
+        # box,
         power, ground,
-        clear, clock,
-        inputs, wires, outputs,
+        clear, clock, preset,
+        jk_in, wires, outputs,
         labels,
 
         stroke("black"), linewidth(2mm)
